18c8bf6dacbfbf9269417ac8ac9a3c11cf4439c1
Francis Kyle V. Sumbang <kyle6301@gmail.com>
2018-02-10 12:53:53 +0800

    deployment

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@
 const crypto = require('crypto').randomBytes(256).toString('hex'); // Provides cryptographic functionality (OpenSSL's hash, HMAC, cipher, decipher, sign and verify functions)
 
 module.exports = {
- uri: 'mongodb://localhost:27017/meandb', // Databse URI and database name
+ uri: 'mongodb://kyledb:elykdb@ds131258.mlab.com:31258/meandb', // Databse URI and database name: Old >  mongodb://localhost:27017/meandb
  secret: crypto, // Cryto-created secret
  db: 'meandb' // Database name
 }
